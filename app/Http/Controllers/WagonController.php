47641ca6c030e85ec67a22f92d73a1d58bf2d116
selenge <selengetu@gmail.com>
2022-02-28 10:28:17 +0800

    controller

--- app/Http/Controllers/WagonController.php
+++ app/Http/Controllers/WagonController.php
@@ -15,15 +15,15 @@ class WagonController extends Controller {
     public function index()
     {
         $id=Auth::user()->id;
-        $categories= DB::select("select * from CATEGORIES t");
-        $companies= DB::select("select * from COMPANIES t");
-        $contract_type= DB::select("select * from CONTRACT_TYPES t");
-        $countries= DB::select("select * from COUNTRIES t");
-        $factories= DB::select("select * from FACTORIES t");
-        $models= DB::select("select * from MODELS t");
-        $railways= DB::select("select * from RAILWAYS t");
-        $wagon_depos= DB::select("select * from WAGON_DEPOS t");
-        $wagons= DB::select("select * from V_WAGONS t");
+        $categories= DB::select("select * from SET_WAGON_CATEGORIES t");
+        $companies= DB::select("select * from COMPANY_INFO t");
+        $contract_type= DB::select("select * from SET_CONTRACT_TYPES t");
+        $countries= DB::select("select * from SET_COUNTRIES t");
+        $factories= DB::select("select * from SET_FACTORIES t");
+        $models= DB::select("select * from SET_WAGON_MODELS t");
+        $railways= DB::select("select * from SET_RAILWAYS t");
+        $wagon_depos= DB::select("select * from SET_DEPOS t");
+        $wagons= DB::select("select * from COMPANY_WAGONS t");
         return view('home', compact('categories','companies','contract_type','countries','factories','models','railways','models','wagon_depos','wagons'));
     }
     public function savewagon(Request $request)
